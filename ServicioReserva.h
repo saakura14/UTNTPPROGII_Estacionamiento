c7b341451e443b02a2d3e9e14c4dbf2df11499bd
Valeria Gil <60628850+saakura14@users.noreply.github.com>
2024-10-24 20:28:15 -0300

    Update ServicioReserva.h
    Base de Clase reserva - pulir

--- ServicioReserva.h
+++ ServicioReserva.h
@@ -1,7 +1,15 @@
 #pragma once
 #include "Fecha.h"
 
-class servicioReserva{
-private:
-
+class Reserva {
+public:
+    char IDReserva[15];
+    char IDEmpleado[15];
+    char IDCliente[15];
+    int IDEstacionamiento;
+    // Suponiendo una estructura para Fecha en lugar de una clase
+    int DiaIngreso, MesIngreso, AnoIngreso;
+    int Dias;
+    float ImporteTotal;
+    bool Estado;
 };
